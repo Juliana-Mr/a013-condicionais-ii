ecb0a02bbe68ab620cf5dc4cf62e1791eeb95822
Juliana <juliana-marques-@hotmail.com>
2022-08-31 14:35:17 -0300

    Exercício 13 is done

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,19 @@
-const num = //seu numero aqui
+const num = +prompt("Digite um número aleatório")
 
+//1) utilizando ifs aninhados
+
+if(num % 2 === 0){
+    if(num % 3 === 0){
+     console.log("Divisível por 2 ou 3")
+    }
+}
+
+//COMO SE FAZ UMA OPERAÇÕA DE "OU" 
+//DENTRO DOS IFs ANINHADOS?
+// do jeito que está só funciona operações de "e"
+
+//2) utilizando operador lógico
+
+if(num % 2 ===0 || num % 3 ===0){
+    console.log("O número é divisível por 2 ou 3")
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,20 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+const nome = "Maria"
+console.log(nome === "José" ? "Oi, Zé!" : "Olá", nome)
+
+let idade = 30
+console.log(idade >= 18 ? "Pode tirar carteira de motorista" :
+                        "Ainda não pode tirar carteira de motorista")
